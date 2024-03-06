49fa7b70de67a4d61fbece860c8dc030c78b2586
Rahman Hafidzin <rhafidzin@gmail.com>
2024-03-06 08:57:39 +0700

    upload basic collection

---
+++ gen-20-java-basic-collection/src/Collections/List.java
@@ -0,0 +1,50 @@
+package Collections;
+
+import java.util.*;
+
+
+public class List {
+
+    public static void inputData(ArrayList<String> listData){
+        System.out.print("Input panjang data:");
+        Scanner input = new Scanner(System.in);
+
+        Integer panjangData = Integer.parseInt(input.nextLine());
+
+        //input data ke listData sesuai dengan panjang data
+        for(int i = 0; i < panjangData; i++){
+            System.out.println("Data ke-" + (i + 1) + ": ");
+            listData.add(input.nextLine());
+        }
+
+
+    }
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<String> listData = new ArrayList<>();
+        inputData(listData);
+
+        // mengeluarkan output array list
+        System.out.println("Berikut output array list");
+        for(String list : listData){
+            System.out.println(list);
+        }
+
+        // menggunakan hash set
+        HashSet<String> dataHash = new HashSet<>(listData);
+        System.out.println("Berikut output hash set");
+        for(String data : dataHash){
+            System.out.println(data);
+        }
+
+        // menggunakan tree set
+        TreeSet<String> dataTree = new TreeSet<>(listData);
+        System.out.println("Berikut output tree set:");
+        for (String tree : dataTree){
+            System.out.println(tree);
+        }
+
+
+    }
+}
