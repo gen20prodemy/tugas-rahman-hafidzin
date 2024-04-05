b65f028bf31b1c9f747bd4c5de0208c8f900e9b7
Rahman Hafidzin <rhafidzin@gmail.com>
2024-04-05 11:10:10 +0700

    add kafka consumer

--- gen-20-java-springboot-pos-consumer/src/main/java/kafka/consumer/MessageConsumer.java
+++ gen-20-java-springboot-pos-consumer/src/main/java/kafka/consumer/MessageConsumer.java
@@ -1,2 +1,22 @@
-package kafka.consumer;public class MessageConsumer {
+package kafka.consumer;
+
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageConsumer {
+
+    @KafkaListener(topics = "myFirstTopic", groupId = "my-group-id")
+    public void listen (String message){
+        System.out.println("Pesan diterima: " + message);
+    }
+
+//    @KafkaListener(topics = "myFirstTopic", groupId = "my-group-id")
+//    public void listenCost (Object message){
+//        System.out.println("Pesan diterima: " + message);
+//    }
+
+
 }
